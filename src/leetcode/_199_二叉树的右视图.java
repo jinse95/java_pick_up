5a0cd523ec831ad2b5e1f23517a8a0d71c835eaf
JiJiaJian <jijj@jezoe.com>
2018-09-29 18:17:54 +0800

    add

---
+++ src/leetcode/_199_二叉树的右视图.java
@@ -0,0 +1,57 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * @Description 考察二叉树按层次遍历
+ * @Author J
+ * @Date 2018/9/29 16:40
+ **/
+public class _199_二叉树的右视图 {
+    public static List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        list.add(root.val);
+
+        while (true) {
+            Queue<TreeNode> tempQueue = new LinkedList<>();
+            Integer last = null;
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    tempQueue.offer(node.left);
+                    last = node.left.val;
+                }
+                if (node.right != null) {
+                    tempQueue.offer(node.right);
+                    last = node.right.val;
+                }
+            }
+            if (tempQueue.isEmpty()) {
+                break;
+            }
+            queue = tempQueue;
+            list.add(last);
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
